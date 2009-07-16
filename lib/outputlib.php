127bd0600f8907bee3f083bf324ea07d15efcb17
ajv <ajv>
2009-07-15 21:15:09 -0500

    MDL-18444 Fixed tabs and spaces for style coherence

--- lib/outputlib.php
+++ lib/outputlib.php
@@ -3139,22 +3139,22 @@ function output_css_for_css_edit($files, $toreplace) {
  * @return string $menu The modified menu with the offline option
  */
 function output_offline_mode($menu) {
-	
-	global $PAGE, $CFG;
-	
-	$PAGE->requires->string_for_js('gooffline', 'moodle');
-	$PAGE->requires->string_for_js('goofflinetitle', 'moodle');
-	$PAGE->requires->string_for_js('goonline', 'moodle');
-	$PAGE->requires->string_for_js('goonlinetitle', 'moodle');
-	$PAGE->requires->string_for_js('pleasewait', 'moodle');
-	$PAGE->requires->string_for_js('cantdetectconnection', 'moodle');
-	$PAGE->requires->string_for_js('mustinstallgears', 'moodle');
-	$PAGE->requires->string_for_js('unavailableextlink', 'moodle');
-	$PAGE->requires->string_for_js('unavailablefeature', 'moodle');
-	
-	$PAGE->requires->js('lib/offline/gears_init.js');
-	$PAGE->requires->js('lib/offline/go_offline.js');
-	$PAGE->requires->js_function_call('init_offline');
-	$menu = '<span id="offline-message"></span><span id="offline-img"></span> <span id="offline-status"><a href="###" onclick="createStore()" title="'.get_string('goofflinetitle').'">'.get_string('gooffline').'</a></span>'.$menu;
-	return $menu;
+    
+    global $PAGE, $CFG;
+    
+    $PAGE->requires->string_for_js('gooffline', 'moodle');
+    $PAGE->requires->string_for_js('goofflinetitle', 'moodle');
+    $PAGE->requires->string_for_js('goonline', 'moodle');
+    $PAGE->requires->string_for_js('goonlinetitle', 'moodle');
+    $PAGE->requires->string_for_js('pleasewait', 'moodle');
+    $PAGE->requires->string_for_js('cantdetectconnection', 'moodle');
+    $PAGE->requires->string_for_js('mustinstallgears', 'moodle');
+    $PAGE->requires->string_for_js('unavailableextlink', 'moodle');
+    $PAGE->requires->string_for_js('unavailablefeature', 'moodle');
+    
+    $PAGE->requires->js('lib/offline/gears_init.js');
+    $PAGE->requires->js('lib/offline/go_offline.js');
+    $PAGE->requires->js_function_call('init_offline');
+    $menu = '<span id="offline-message"></span><span id="offline-img"></span> <span id="offline-status"><a href="###" onclick="createStore()" title="'.get_string('goofflinetitle').'">'.get_string('gooffline').'</a></span>'.$menu;
+    return $menu;
 }
